80bed4d85730d2d117645413dcfe7dcaf5bb482a
JianyYang <betterlife.young@gmail.com>
2023-02-04 20:05:34 +0800

    Update AutoCheckinNew.py

--- AutoCheckinNew.py
+++ AutoCheckinNew.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
         	
         logindata = {
             "email": username,
-            "passwd": passwd
+            "passwd": password
         }
         loginheaders = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/107.0.1418.52",
